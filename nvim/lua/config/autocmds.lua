7d8349ea73d2e8a3b5abb760e03ceb28bf21d41f
Felix Achter <felix.achter.it@gmail.com>
2026-08-28 22:55:07 +0200

    feat: navigation between spaces and visual/editor mode in herdr

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Herdr prefix+e writes pane scrollback to a temp file like
+-- herdr-scrollback-<pid>-<nanos>-<attempt>.txt
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("herdr_scrollback", { clear = true }),
+  pattern = "*/herdr-scrollback-*.txt",
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = true
+  end,
+})
